ff1890f98a83dff1ecb636c2ed5f13d2f93e16e9
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-06-04 15:36:14 -0300

    finished lesson 3

--- MODULO3-TESTING-BASICS/projects/basic-testing-frontend/src/math.test.js
+++ MODULO3-TESTING-BASICS/projects/basic-testing-frontend/src/math.test.js
@@ -1,4 +1,15 @@
 // import { test } from 'vitest';  // é possível deixar de importar diretamente o 'test' do vitest, se vc escrever '--globals' lá no run de 'npm test', lá na package.json do seu project... mas isso não é recomendado, pq o suporte do IDE sem o impotrt não é tão bom..
 // ? 'test' É A MESMA COISA QUE A FUNCTION DE 'it()'...
 
-import { it } from 'vitest';
+import { it, expect } from 'vitest';
+
+import { add } from './math.js'; // DEVEMOS USAR/RODAR NOSSA FUNCTION DENTRO DO TEST, COM 'expect().toBe()'...
+
+it('should summarize all number values in an array', () => {
+  const result = add([1, 2, 3, 4, 5]);
+  expect(result).toBe(15);
+//   expect(result).toBe(6);
+
+
+  expect(result).toBe
+});
