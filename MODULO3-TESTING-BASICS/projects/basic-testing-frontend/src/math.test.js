dc3438f5e60eb7564f5f885fee9624c217d261c9
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-06-04 15:20:53 -0300

    finished lesson 2

---
+++ MODULO3-TESTING-BASICS/projects/basic-testing-frontend/src/math.test.js
@@ -0,0 +1,4 @@
+// import { test } from 'vitest';  // é possível deixar de importar diretamente o 'test' do vitest, se vc escrever '--globals' lá no run de 'npm test', lá na package.json do seu project... mas isso não é recomendado, pq o suporte do IDE sem o impotrt não é tão bom..
+// ? 'test' É A MESMA COISA QUE A FUNCTION DE 'it()'...
+
+import { it } from 'vitest';
