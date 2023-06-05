f9659c87a33cf265443c7d29dfc1804b0281205c
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-06-05 02:02:23 -0300

    finished lesson 17

--- MODULO3-TESTING-BASICS/projects/basic-testing-frontend/src/math.test.js
+++ MODULO3-TESTING-BASICS/projects/basic-testing-frontend/src/math.test.js
@@ -1,115 +1,117 @@
 // import { test } from 'vitest';  // é possível deixar de importar diretamente o 'test' do vitest, se vc escrever '--globals' lá no run de 'npm test', lá na package.json do seu project... mas isso não é recomendado, pq o suporte do IDE sem o impotrt não é tão bom..
 // ? 'test' É A MESMA COISA QUE A FUNCTION DE 'it()'...
 
-import { it, expect } from 'vitest';
+import { it, expect, describe } from 'vitest';
 
 import { add } from './math.js'; // DEVEMOS USAR/RODAR NOSSA FUNCTION DENTRO DO TEST, COM 'expect().toBe()'...
 
-it('should summarize all number values in an array', () => {
-  //   const result = add([1, 2, 3, 4, 5]);
-  //   expect(result).toBe(15);
-  //   expect(result).toBe(6);
+describe('add', () => {
+  it('should summarize all number values in an array', () => {
+    //   const result = add([1, 2, 3, 4, 5]);
+    //   expect(result).toBe(15);
+    //   expect(result).toBe(6);
 
-  // TODO - DEVEMOS SEGUIR O PADRÃO DE 3 FASES (para deixar o test mais legível e fácil de entender)...):
+    // TODO - DEVEMOS SEGUIR O PADRÃO DE 3 FASES (para deixar o test mais legível e fácil de entender)...):
 
-  // 1 - ARRANGE
-  const numbers = [1, 2, 3];
-  const expectedResult = numbers.reduce((prevValue, curValue) => {
-    return prevValue + curValue;
-  }, 0);
+    // 1 - ARRANGE
+    const numbers = [1, 2, 3];
+    const expectedResult = numbers.reduce((prevValue, curValue) => {
+      return prevValue + curValue;
+    }, 0);
 
-  // 2 - ACT
-  const result = add(numbers);
+    // 2 - ACT
+    const result = add(numbers);
 
-  // 3 - ASSERT
-  expect(result).toBe(expectedResult);
-});
+    // 3 - ASSERT
+    expect(result).toBe(expectedResult);
+  });
 
-it('should yield NaN if at least one invalid number is provided', () => {
-  // ARRANGE
+  it('should yield NaN if at least one invalid number is provided', () => {
+    // ARRANGE
 
-  const values = [1, 'string'];
+    const values = [1, 'string'];
 
-  // ACT
+    // ACT
 
-  const result = add(values);
+    const result = add(values);
 
-  // ASSERT
+    // ASSERT
 
-  expect(result).toBeNaN();
-});
+    expect(result).toBeNaN();
+  });
 
-it('should yield a correct sum if an array of numeric string values is provided', () => {
-  // ARRANGE
+  it('should yield a correct sum if an array of numeric string values is provided', () => {
+    // ARRANGE
 
-  const values = ['1', '2', '3'];
-  const expectedResult = values.reduce((prevValue, curValue) => {
-    return +prevValue + +curValue; ///queremos um REAL NUMBER, por isso o '+prevValue' e o '+curValue'...
-  }, 0);
+    const values = ['1', '2', '3'];
+    const expectedResult = values.reduce((prevValue, curValue) => {
+      return +prevValue + +curValue; ///queremos um REAL NUMBER, por isso o '+prevValue' e o '+curValue'...
+    }, 0);
 
-  // ACT
+    // ACT
 
-  const result = add(values);
+    const result = add(values);
 
-  // ASSERT
+    // ASSERT
 
-  expect(result).toBe(expectedResult);
-});
+    expect(result).toBe(expectedResult);
+  });
 
-it('should yield 0 if an empty array is provided', () => {
-  // ARRANGE
-  const values = [];
+  it('should yield 0 if an empty array is provided', () => {
+    // ARRANGE
+    const values = [];
 
-  // ACT
-  const result = add(values);
+    // ACT
+    const result = add(values);
 
-  // ASSERT
-  expect(result).toBe(0);
-});
+    // ASSERT
+    expect(result).toBe(0);
+  });
 
-it('should throw an error if no argument is passed into the function', () => {
-  // TODO - É ASSIM QUE __ FAZEMOS __ PARA TESTAR SE UMA FUNCTION LANÇA UM ERRO:
+  it('should throw an error if no argument is passed into the function', () => {
+    // TODO - É ASSIM QUE __ FAZEMOS __ PARA TESTAR SE UMA FUNCTION LANÇA UM ERRO:
 
-  // ARRANGE (AND ACT)
-  const resultFn = () => {
-    add();
-  };
+    // ARRANGE (AND ACT)
+    const resultFn = () => {
+      add();
+    };
 
-  // ASSERT
-  expect(resultFn).toThrowError('No numbers provided.');
-});
+    // ASSERT
+    expect(resultFn).toThrowError('No numbers provided.');
+  });
 
-it('should yield something that is not a string, if numbers in string format are provided', () => {
-  // ARRANGE
-  const values = ['1', '2'];
+  it('should yield something that is not a string, if numbers in string format are provided', () => {
+    // ARRANGE
+    const values = ['1', '2'];
 
-  // ACT
-  const result = add(values);
+    // ACT
+    const result = add(values);
 
-  // ASSERT
-  expect(result).not.toBeTypeOf('string');
-});
+    // ASSERT
+    expect(result).not.toBeTypeOf('string');
+  });
 
-it('should throw an error if provided with multiple arguments instead of an array', () => {
-  // ARRANGE
-  const [value1, value2, value3] = [1, 2, 3];
-  // ACT
-  const resultFn = () => {
-    add(value1, value2, value3);
-  };
-  // ASSERT
-  expect(resultFn).toThrowError(
-    'Please provide an array of numbers, instead of multiple number-arguments'
-  );
-});
+  it('should throw an error if provided with multiple arguments instead of an array', () => {
+    // ARRANGE
+    const [value1, value2, value3] = [1, 2, 3];
+    // ACT
+    const resultFn = () => {
+      add(value1, value2, value3);
+    };
+    // ASSERT
+    expect(resultFn).toThrowError(
+      'Please provide an array of numbers, instead of multiple number-arguments'
+    );
+  });
 
-it('should throw an error if provided with multiple arguments instead of an array', () => {
-  // ARRANGE
-  const [value1, value2, value3] = [1, 2, 3];
-  // ACT
-  const resultFn = () => {
-    add(value1, value2, value3);
-  };
-  // ASSERT
-  expect(resultFn).toThrowError(/multiple number-arguments/); // --> podemos usar uma REGEX, aqui, para testar se a mensagem de erro contém uma certa string...
+  it('should throw an error if provided with multiple arguments instead of an array', () => {
+    // ARRANGE
+    const [value1, value2, value3] = [1, 2, 3];
+    // ACT
+    const resultFn = () => {
+      add(value1, value2, value3);
+    };
+    // ASSERT
+    expect(resultFn).toThrowError(/multiple number-arguments/); // --> podemos usar uma REGEX, aqui, para testar se a mensagem de erro contém uma certa string...
+  });
 });
--- MODULO3-TESTING-BASICS/projects/basic-testing-frontend/src/util/validation.test.js
+++ MODULO3-TESTING-BASICS/projects/basic-testing-frontend/src/util/validation.test.js
@@ -1,110 +1,114 @@
-import { it, expect } from 'vitest';
+import { it, expect, describe } from 'vitest';
 
 import { validateNumber, validateStringNotEmpty } from './validation';
+/// * exemplo de 'TEST SUITE' (com 'describe')
+describe('validateStringNotEmpty', () => {
+  it('should return no value if a non-empty string is provided', () => {
+    //ARRANGE
+    const value = '1';
+    //ACT
+    const result = validateStringNotEmpty(value);
+    //ASSERT
+    expect(result).toBeUndefined(); // * 'toBeUndefined' é usado em casos em que FUNCTIONS NÃO RETORNAM COISA ALGUMA (tipo esta function, nos casos de success)...
+  });
 
-it('should return no value if a non-empty string is provided', () => {
-  //ARRANGE
-  const value = '1';
-  //ACT
-  const result = validateStringNotEmpty(value);
-  //ASSERT
-  expect(result).toBeUndefined(); // * 'toBeUndefined' é usado em casos em que FUNCTIONS NÃO RETORNAM COISA ALGUMA (tipo esta function, nos casos de success)...
+  it('should not throw an error, if a non-empty string is provided 2', () => {
+    // professor...
+    const input = 'valid';
+    const validationFn = () => validateStringNotEmpty(input);
+    expect(validationFn).not.toThrow();
+  });
+
+  it('should throw an error upon being called with an empty string as argument', () => {
+    //ARRANGE
+    const value = '';
+    //ACT
+    const resultFn = () => {
+      validateStringNotEmpty(value);
+    };
+    //ASSERT
+    expect(resultFn).toThrowError('Invalid input - must not be empty.');
+  });
+
+  it('should throw an error if a long string of blanks is provided', () => {
+    const input = '   ';
+    const validationFn = () => validateStringNotEmpty(input);
+    expect(validationFn).toThrowError('Invalid input - must not be empty.');
+  });
+
+  it('should throw an undefined error upon being called with no arguments', () => {
+    //ARRANGE
+
+    //ACT
+    const resultFn = () => {
+      validateStringNotEmpty();
+    };
+    //ASSERT
+    expect(resultFn).toThrowError(/undefined/); // * regex - "undefined" pode estar em qualquer lugar da mensagem de erro (no caso, 'value is undefined')
+  });
+
+  it('should throw an error if any other value than a string is provided', () => {
+    const inputNum = 1;
+    const inputBool = true;
+    const inputObj = {};
+
+    const validationFnNum = () => validateStringNotEmpty(inputNum);
+    const validationFnBool = () => validateStringNotEmpty(inputBool);
+    const validationFnObj = () => validateStringNotEmpty(inputObj);
+
+    expect(validationFnNum).toThrow();
+    expect(validationFnBool).toThrow();
+    expect(validationFnObj).toThrow();
+  });
 });
 
-it('should not throw an error, if a non-empty string is provided 2', () => {
-  // professor...
-  const input = 'valid';
-  const validationFn = () => validateStringNotEmpty(input);
-  expect(validationFn).not.toThrow();
-});
+describe('validateNumber', () => {
+  it('should throw an error if NaN is provided', () => {
+    const input = NaN;
+    const validationFn = () => validateNumber(input);
+    expect(validationFn).toThrow();
+  });
 
-it('should throw an error upon being called with an empty string as argument', () => {
-  //ARRANGE
-  const value = '';
-  //ACT
-  const resultFn = () => {
-    validateStringNotEmpty(value);
-  };
-  //ASSERT
-  expect(resultFn).toThrowError('Invalid input - must not be empty.');
-});
+  it('should return no value if a number is provided', () => {
+    //ARRANGE
+    const value = 1;
+    //ACT
+    const result = validateNumber(value);
+    //ASSERT
+    expect(result).toBeUndefined();
+  });
 
-it('should throw an error if a long string of blanks is provided', () => {
-  const input = '   ';
-  const validationFn = () => validateStringNotEmpty(input);
-  expect(validationFn).toThrowError('Invalid input - must not be empty.');
-});
+  it('should not throw an error, if a non-empty string is provided', () => {
+    //professor...
+    const input = 'valid';
+    const validationFn = () => validateStringNotEmpty(input);
+    expect(validationFn).not.toThrow();
+  });
 
-it('should throw an undefined error upon being called with no arguments', () => {
-  //ARRANGE
+  it('should throw an undefined error upon being called with no arguments', () => {
+    //ARRANGE
+    //ACT
+    const resultFn = () => {
+      validateNumber();
+    };
+    //ASSERT
+    expect(resultFn).toThrowError(/No value provided/);
+  });
 
-  //ACT
-  const resultFn = () => {
-    validateStringNotEmpty();
-  };
-  //ASSERT
-  expect(resultFn).toThrowError(/undefined/); // * regex - "undefined" pode estar em qualquer lugar da mensagem de erro (no caso, 'value is undefined')
-});
-
-it('should throw an error if any other value than a string is provided', () => {
-  const inputNum = 1;
-  const inputBool = true;
-  const inputObj = {};
-
-  const validationFnNum = () => validateStringNotEmpty(inputNum);
-  const validationFnBool = () => validateStringNotEmpty(inputBool);
-  const validationFnObj = () => validateStringNotEmpty(inputObj);
-
-  expect(validationFnNum).toThrow();
-  expect(validationFnBool).toThrow();
-  expect(validationFnObj).toThrow();
-});
-
-it('should throw an error if NaN is provided', () => {
-  const input = NaN;
-  const validationFn = () => validateNumber(input);
-  expect(validationFn).toThrow();
-});
-
-it('should return no value if a number is provided', () => {
-  //ARRANGE
-  const value = 1;
-  //ACT
-  const result = validateNumber(value);
-  //ASSERT
-  expect(result).toBeUndefined();
-});
-
-it('should not throw an error, if a non-empty string is provided', () => {
-  //professor...
-  const input = 'valid';
-  const validationFn = () => validateStringNotEmpty(input);
-  expect(validationFn).not.toThrow();
-});
-
-it('should throw an undefined error upon being called with no arguments', () => {
-  //ARRANGE
-  //ACT
-  const resultFn = () => {
-    validateNumber();
-  };
-  //ASSERT
-  expect(resultFn).toThrowError(/No value provided/);
-});
-
-it('should throw an error if the value provided is not a number', () => {
-  //ARRANGE
-  const value = 'exemplo';
-  const value2 = '2';
-  //ACT
-  const resultFn = () => {
-    validateNumber(value);
-  };
-  const resultFn2 = () => {
-    validateNumber(value2);
-  };
-  //ASSERT
-  expect(resultFn).toThrowError('Invalid number input.');
-  //   expect(resultFn2).toThrowError('Invalid number input.');
-  expect(resultFn2).toThrow();
+  it('should throw an error if the value provided is not a number', () => {
+    //ARRANGE
+    const value = 'exemplo';
+    const value2 = '2';
+    //ACT
+    const resultFn = () => {
+      validateNumber(value);
+    };
+    const resultFn2 = () => {
+      validateNumber(value2);
+    };
+    //ASSERT
+    expect(resultFn).toThrowError('Invalid number input.');
+    //   expect(resultFn2).toThrowError('Invalid number input.');
+    expect(resultFn2).toThrow();
+  });
 });
