7672f6b452abdd2848a47b7ea58f6018b0f31dfc
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-06-07 15:26:01 -0300

    finished lesson 2

---
+++ MODULO9-TESTING-E-O-DOM/projeto-dom-browserside/util/dom.test.js
@@ -0,0 +1,8 @@
+import { it, expect, describe } from 'vite';
+import { showError } from './dom';
+
+describe('first test', () => { //ISSO FALHARÁ --> FALHARÁ PQ NÃO TEMOS, NATIVAMENTE, ACESSO AO 'DOM' COM O VITEST...
+  it('first test', () => { //ganharemos 'document is not defined'...
+    showError('some message');
+  });
+});
