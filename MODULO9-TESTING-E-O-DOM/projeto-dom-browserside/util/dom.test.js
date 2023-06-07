fdda70026568e6b481fbe4a066fd1f6fa594a56e
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-06-07 16:43:30 -0300

    finished lesson 6

--- MODULO9-TESTING-E-O-DOM/projeto-dom-browserside/util/dom.test.js
+++ MODULO9-TESTING-E-O-DOM/projeto-dom-browserside/util/dom.test.js
@@ -1,5 +1,6 @@
 import { it, expect, describe } from 'vitest';
 import { vi } from 'vitest';
+import { beforeEach } from 'vitest';
 import { showError } from './dom';
 import { Window } from 'happy-dom';
 
@@ -9,12 +10,15 @@ import path from 'path'; // usado para IMPORTAR 1 FILE NO VIRTUAL DOM PRODUZIDO 
 const htmlDocPath = path.join(process.cwd(), 'index.html'); //path de noso arquivo html
 const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString(); //conteúdo de nosso arquivo html
 
+// let window; //criamos 1 'window' (1 DOM VIRTUAL) com o 'happy-dom'...
+// let document; //criamos 1 'document' (1 DOM VIRTUAL) com o 'happy-dom'...
+
 const window = new Window(); //criamos 1 'window' (1 DOM VIRTUAL) com o 'happy-dom'...
 const document = window.document; //criamos 1 'document' (1 DOM VIRTUAL) com o 'happy-dom'...
-
-document.write(htmlDocumentContent); //escrevemos o conteúdo de nosso arquivo html no 'document' (1 DOM VIRTUAL) criado com o 'happy-dom'...
-
 vi.stubGlobal('document', document); //criamos 1 stub global para o 'document' (1 DOM VIRTUAL) criado com o 'happy-dom'...
+// document.write(htmlDocumentContent); //escrevemos o conteúdo de nosso arquivo html no 'document' (1 DOM VIRTUAL) criado com o 'happy-dom'...
+
+// vi.stubGlobal('document', document); //criamos 1 stub global para o 'document' (1 DOM VIRTUAL) criado com o 'happy-dom'...
 
 // describe('first test', () => {
 //   ISSO FALHARÁ --> FALHARÁ PQ NÃO TEMOS, NATIVAMENTE, ACESSO AO 'DOM' COM O VITEST...
@@ -25,6 +29,19 @@ vi.stubGlobal('document', document); //criamos 1 stub global para o 'document' (
 // });
 
 describe('showError', () => {
+  beforeEach(() => {
+    //exemplo de 'cleanup work', para realizar múltiplos tests sobre o mesmo document...
+
+    // window = new Window(); //! versão menos elegante de 'reset' do dom a partir de cada test.
+    // document = window.document;
+    // document.write(htmlDocumentContent); //escrevemos o conteúdo de nosso arquivo html no 'document' (1 DOM VIRTUAL) criado com o 'happy-dom'...
+    // vi.stubGlobal('document', document); //criamos 1 stub global para o 'document' (1 DOM VIRTUAL) criado com o 'happy-dom'...
+
+    //? VERSÃO MAIS ELEGANTE DE 'reset' do dom a partir de cada test
+    document.body.innerHTML = ''; //limpamos o 'body' do 'document' (1 DOM VIRTUAL) criado com o 'happy-dom'...
+    document.write(htmlDocumentContent); //escrevemos o conteúdo de nosso arquivo html no 'document' (1 DOM VIRTUAL) criado com o 'happy-dom'...
+  });
+
   it('should add an error paragraph to the id="errors" element', () => {
     //ARRANGE
     const message = 'some error';
@@ -35,13 +52,27 @@ describe('showError', () => {
     //ASSERT
     expect(errorParagraph).not.toBeNull(); //verificamos SE ESSE ELEMENT NÃO ESTÁ COMO 'null' (ou seja, se ele EXISTE)...
   });
+
+  // --> SE O ELEMENTO PARAGRAPH NÃO EXISTISSE DENTRO
+
+  // DAQUELE ELEMENT,
+
+  // ELE SERIA 'null' (
+
+  //   é assim que fazemos, para checar se coisas FORAM RENDERIZADAS NO DOM...
+  // )
+
+  it('should not contain an error paragraph, initially', () => {
+    //ARRANGE
+    const message = 'some error';
+    const errorsElement = document.getElementById('errors');
+    let errorParagraph = errorsElement.firstElementChild;
+    //ASSERT
+    expect(errorParagraph).toBeNull(); //verificamos SE ESSE ELEMENT ESTÁ COMO 'null' INICIALMENTE (ou seja, se ele EXISTE)...
+    //ACT
+    showError(message);
+    errorParagraph = errorsElement.firstElementChild;
+    //ASSERT
+    expect(errorParagraph).not.toBeNull();
+  });
 });
-
-// --> SE O ELEMENTO PARAGRAPH NÃO EXISTISSE DENTRO
-
-// DAQUELE ELEMENT,
-
-// ELE SERIA 'null' (
-
-//   é assim que fazemos, para checar se coisas FORAM RENDERIZADAS NO DOM...
-// )
